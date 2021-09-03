e18c277df2d519bea82c3f38e6fa2fbf1cceaabb
aidarous <aidarous12@gmail.com>
2021-09-03 19:31:19 -0400

    added nav bar with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 function App() {
+  
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <nav>
+            <Link to="/Homepage">Home  </Link>
+            <Link to="/ToDoList"> ToDo list </Link>
+            <Link to="/AddToDo"> Add Todo</Link>
+          </nav>
+        </div>
+      </Router>
     </div>
   );
 }
